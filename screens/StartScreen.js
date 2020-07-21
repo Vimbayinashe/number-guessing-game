43ef8446d07e41260f48014cb21b471bb057c359
Vimbayinashe <vimbayinashemandaza@gmail.com>
2020-07-21 19:25:20 +0200

    Style start screen

--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 
 const StartScreen = (props) => {
    
 return(
     <View style={styles.screen}>
-        <Text>The Game Screen!</Text>
+        <Text style={styles.title}>Start a New Game!</Text>
+        <View style={styles.inputContainer}>
+            <Text>Select a Number</Text>
+            <TextInput />
+            <View style={styles.buttonContainer}>
+                <Button title="Reset" onPress={() => {}} />
+                <Button title="Confirm" onPress={() => {}} />
+            </View>
+        </View>
     </View>
 )
 };
@@ -15,6 +23,29 @@ const styles = StyleSheet.create({
        flex: 1,  //object takes up entire space
        padding: 10,
        alignItems: 'center'
+   },
+   title: {
+        fontSize: 20,
+        marginVertical: 10 //=> marginTop & marginBottom
+   },
+   inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center',
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 6,
+        shadowOpacity: 0.26,
+        elevation: 5,
+        backgroundColor: 'white',
+        padding: 20,
+        borderRadius: 10
+   },
+   buttonContainer: {
+       flexDirection: 'row',
+       width: '100%',
+       justifyContent: 'space-between',
+       paddingHorizontal: 15
    }
 });
 
