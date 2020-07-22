64f6937e92d4bd72ebc983567bab30ca4f0cd01c
Vimbayinashe <vimbayinashemandaza@gmail.com>
2020-07-22 14:31:02 +0200

    Create constants folder

--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 import Card from '../components/Card';
+import Colors from '../constants/colors';
 
 const StartScreen = (props) => {
    
@@ -11,8 +12,12 @@ return(
             <Text>Select a Number</Text>
             <TextInput />
             <View style={styles.buttonContainer}>
-                <Button title="Reset" onPress={() => {}} />
-                <Button title="Confirm" onPress={() => {}} />
+                <View style={styles.button}>
+                    <Button color={Colors.accent} title="Reset" onPress={() => {}} />
+                </View>
+                <View style={styles.button}>
+                    <Button color={Colors.primary} title="Confirm" onPress={() => {}} />
+                </View>
             </View>
         </Card>
     </View>
@@ -39,6 +44,10 @@ const styles = StyleSheet.create({
        width: '100%',
        justifyContent: 'space-between',
        paddingHorizontal: 15
+   },
+   button: {
+       borderRadius: 50,
+       width: '40%'
    }
 });
 
